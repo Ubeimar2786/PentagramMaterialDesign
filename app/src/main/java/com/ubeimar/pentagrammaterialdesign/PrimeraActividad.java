c9f2d49c3fba31ecb9278586994af5853c484a6b
ubeimar2786 <ubeimar.com@hotmail.com>
2016-11-04 12:45:06 -0500

    Initial commit

---
+++ app/src/main/java/com/ubeimar/pentagrammaterialdesign/PrimeraActividad.java
@@ -0,0 +1,13 @@
+package com.ubeimar.pentagrammaterialdesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrimeraActividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primera_actividad);
+    }
+}
